84ac486ccdd7508de639b511e08ee5e4bd42a1c2
Victor <victo.elcatire75@gmail.com>
2025-05-20 15:21:08 -0500

    instalacion de tailwind y materialtailwind

---
+++ Front-End/Gestion_Productos/tailwind.config.js
@@ -0,0 +1,19 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
+/** @type {import('tailwindcss').Config} */
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f1c40f',
+        'secondary': '#1b1b1b',
+      }
+    },
+  },
+  plugins: [],
+})
+
